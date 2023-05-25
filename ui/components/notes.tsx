d9209cf805fcafbc082a40165399cb852c69e059
alamelu1904 <alamelu85@gmail.com>
2023-05-25 20:05:41 +0530

    updated

--- ui/components/notes.tsx
+++ ui/components/notes.tsx
@@ -14,6 +14,9 @@ const Example: React.FC = () => {
   const [msgType, setMsgType] = useState('');
   const [closeWindow, setCloseWindow] = useState(false);
   const [entData, setEntData] = useState<any>(null);
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const [showLoad, setShowLoad] = useState(false);
+
 
   const startLoadMenu = (appList: any) => {
     if (
@@ -36,7 +39,6 @@ const Example: React.FC = () => {
       populateEntitlement(appIdsTemp[0], 0, appIdsTemp, appList);
     }
   };
-
   const populateMenu = (
     appId: string,
     appIdindex: number,
@@ -61,6 +63,51 @@ const Example: React.FC = () => {
         if (appIdsLst.length - 1 > appIdindex) {
           appIdindex = appIdindex + 1;
           let appIDNxt = appIdsLst[appIdindex];
-          let urlNxt = entData.gAppMap[appIDNxt].action;
+          let urlNxt = state.entData.gAppMap[appIDNxt].action;
           console.log('in populateMenu--' + appIDNxt + '--url--' + urlNxt);
-          populateMenu(appIDNxt, appIdindex, urlNxt, appIds
+          populateMenu(appIDNxt, appIdindex, urlNxt, appIdsLst, entitleData);
+        } else {
+          setLoading(false);
+          setLoginSuccess(true);
+        }
+      })
+      .catch((error) => {
+        console.error('Response Error: ', error);
+        setLoginSuccess(true);
+        setLoading(false);
+        setShowError(true);
+        setErrMsg('Exception occurred. Please contact admin.');
+        setShowLoading(false);
+        setMsgType('ERROR');
+        setCloseWindow(false);
+      });
+  };
+  
+  const populateEnt = (appId: string, appIdindex: number, appIdsLst: string[], appList: any) => {
+    let url = EProps.getContextUrl() + '/abc/M.action';
+    fetch(url + '?version=abc&appId=' + appId, {
+      method: 'post',
+    })
+      .then((res) => {
+        if (!res.ok) throw res;
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // this.loadMenu(data);
+        setEntData(data);
+        let url = data.gAMap[appId].action;
+        populateMenu(appId, appIdindex, url, appIdsLst, appList);
+      })
+      .catch((error) => {
+        console.error('Response Error: ', error);
+        setDisplayMenu(true);
+        setShowLoad(false);
+        setShowError(true);
+        setErrMsg('Exception occurred.');
+        setShowLoading(false);
+        setMsgType('ERROR');
+        setCloseWindow(false);
+      });
+  };
+  
