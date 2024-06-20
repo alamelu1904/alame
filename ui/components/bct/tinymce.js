8194dd0c206c025b03e1c9869286794707905233
alamelu85 <alameluraman19@gmail.com>
2024-06-20 18:08:28 +0530

    updt

--- ui/components/bct/tinymce.js
+++ ui/components/bct/tinymce.js
@@ -1,39 +1,46 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
-const MyEditor = () => {
-  // Initial content can be set to an HTML string
-  const [content, setContent] = useState('<p>Initial <strong>bold</strong> content with a <a href="https://example.com">link</a></p>');
+class MyEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '<p>Initial <strong>bold</strong> content with a <a href="https://example.com">link</a></p>'
+    };
+  }
 
-  const handleEditorChange = (content, editor) => {
-    setContent(content);
+  handleEditorChange = (content, editor) => {
+    this.setState({ content });
     console.log('Content was updated:', content);
   };
 
-  return (
-    <div>
-      <Editor
-        apiKey="YOUR_TINYMCE_API_KEY"
-        value={content}
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-          ],
-          toolbar:
-            'undo redo | formatselect | bold italic backcolor | \
-            alignleft aligncenter alignright alignjustify | \
-            bullist numlist outdent indent | removeformat | help'
-        }}
-        onEditorChange={handleEditorChange}
-      />
-      <h2>Output</h2>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  );
-};
+  render() {
+    const { content } = this.state;
+    return (
+      <div>
+        <Editor
+          apiKey="YOUR_TINYMCE_API_KEY"
+          value={content}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar:
+              'undo redo | formatselect | bold italic backcolor | ' +
+              'alignleft aligncenter alignright alignjustify | ' +
+              'bullist numlist outdent indent | removeformat | help'
+          }}
+          onEditorChange={this.handleEditorChange}
+        />
+        <h2>Output</h2>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    );
+  }
+}
 
 export default MyEditor;
